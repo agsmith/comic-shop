fcec0743e5e518b1a9b8e4017601d75a54dcc8e1
asmith <asmith@ccadllc.us>
2017-09-12 16:51:46 -0400

    Add some ADTs

--- src/main/scala/io/github/agsmith/comicshop/model/ComicBook.scala
+++ src/main/scala/io/github/agsmith/comicshop/model/ComicBook.scala
@@ -1,3 +1,18 @@
 package io.github.agsmith.comicshop.model
 
-case class ComicBook(title: String, authorFirstName: String, authorLastName: String, isbn: String, price: String)
+case class Title(value: String)
+case class AuthorName(value: String)
+case class ISBN(value: String)
+case class Price(value: String)
+
+sealed trait Publisher
+object Marvel extends Publisher
+object DC extends Publisher
+object DarkHorse extends Publisher
+
+case class ComicBook(
+  title: Title,
+  publisher: Publisher,
+  authorName: AuthorName,
+  isbn: ISBN,
+  price: Price)
